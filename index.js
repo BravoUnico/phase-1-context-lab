08563f8e7913e8a564b107ceacfd979882c52d47
Alejandro <sancha0701@gmail.com>
2022-09-07 20:34:15 -0400

    commit tree 5ed93a4f2c3dc6f126816da0fe598a542c4ef201

--- index.js
+++ index.js
@@ -1,4 +1,74 @@
 /* Your Code Here */
+function createEmployeeRecord(record) {
+    const employeeRecords = {
+    firstName: record[0],
+    familyName:record[1],
+    title: record[2],
+    payPerHour: record[3],
+    timeInEvents: [],
+    timeOutEvents: []
+}
+    return employeeRecords
+}
+function createEmployeeRecords(employee) {
+    return employee.map(employee => createEmployeeRecord(employee))
+}
+
+function createTimeInEvent(staff) {
+    let [date, hour] = staff.split(" ")
+    let eventObj = {
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date 
+    }
+
+        this.timeInEvents.push(eventObj)
+        return this
+}
+
+function createTimeOutEvent(event) {
+    let [ date, hour] = event.split(" ")
+    let eventObj = {
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date
+    }
+        this.timeOutEvents.push(eventObj)
+        return this
+}
+
+function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find(staff => staff.date === date)
+    const timeOut = this.timeOutEvents.find(staff => staff.date === date)
+    
+    return (timeOut.hour - timeIn.hour)/100
+
+}
+
+function wagesEarnedOnDate(date) {
+    const hours = hoursWorkedOnDate.call(this, date)
+    return this.payPerHour*hours
+}
+
+function calculatePayroll(employeeRecords) {
+   // const record = createEmployeeRecords.map(employee => allWagesFor.bind(employee))
+
+    return employeeRecords.reduce(( currentValue, current) => currentValue + allWagesFor.call(current), 0)
+}
+
+function findEmployeeByFirstName(employeeRecord, firstName) {
+    console.log(employeeRecord)
+
+    return employeeRecord.find(rec => rec.firstName === firstName)
+}
+
+
+
+//function findEmployeeByFirstName(collection, firstNameString) {
+   // return this.firstName.find( collection => {}
+//}
+
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -8,6 +78,8 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+
+
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
